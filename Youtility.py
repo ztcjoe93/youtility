e69529cc62acacfb2f4805582e39122d8069e68b
joe@nastrond <ztcjoe93@gmail.com>
2022-11-04 00:36:23 +0800

    Added timer decorator function, removed logger propagation, update for existing test cases
    - Uses time.perf_counter() for measurement (non-GC)
    - Added test case for timer decorator
    - Simplified test cases for logger

--- Youtility.py
+++ Youtility.py
@@ -1,46 +1,68 @@
 import logging
 import sys
+import time
+import typing
 
-class Youtility:
-    '''Utility class that contains commonly used functions'''
+def initialize_logger(name: str = None, debug_level: int = 10,
+    filename: str = 'debug.log', console_logging: bool = True,
+    file_logging: bool = True, file_arguments: dict = {}) -> logging.Logger:
+    """A detailed generic logger that can be further configured.
 
-    def initialize_logger(name: str = None, debug_level: int = 10,
-        filename: str = 'debug.log', console_logging: bool = True,
-        file_logging: bool = True, file_arguments: dict = {}) -> logging.Logger:
-        """A detailed generic logger that can be further configured.
+    Returns a pre-configured logger that has common settings, but can be
+    further configured if required by specifying required parameters.
 
-        Returns a pre-configured logger that has common settings, but can be
-        further configured if required by specifying required parameters.
+    Args:
+        name: Name of the logger.
+        debug_level: Debug level for the logger, takes in values in intervals of 10
+            from 0 to 50.
+        filename: Name of the log file.
+        console_logging: Enable log records to be displayed in the console.
+        file_logging: Enable log records to be saved into a log file.
+        file_arguments: Keyword arguments for the file handler.
 
-        Args:
-            name: Name of the logger.
-            debug_level: Debug level for the logger, takes in values in intervals of 10
-                from 0 to 50.
-            filename: Name of the log file.
-            console_logging: Enable log records to be displayed in the console.
-            file_logging: Enable log records to be saved into a log file.
-            file_arguments: Keyword arguments for the file handler.
+    Returns:
+        A Logger class that can be utilized.
+    """
 
-        Returns:
-            A Logger class that can be utilized.
-        """
+    logger = logging.getLogger(name)
+    logger.setLevel(debug_level)
+    # https://stackoverflow.com/questions/6729268/log-messages-appearing-twice-with-python-logging#answer-44426266
+    logger.propagate = False
 
-        logger = logging.getLogger(name)
-        logger.setLevel(debug_level)
+    file_handler = logging.FileHandler(filename, **file_arguments)
+    console_handler = logging.StreamHandler(sys.stdout)
 
-        file_handler = logging.FileHandler(filename, **file_arguments)
-        console_handler = logging.StreamHandler(sys.stdout)
+    for handler in [file_handler, console_handler]:
+        handler.setLevel(debug_level)
+        handler.setFormatter(logging.Formatter(
+            '%(asctime)s [%(threadName)s] %(filename)s.%(funcName)-20s %(levelname)s  %(message)s'
+        ))
 
-        for handler in [file_handler, console_handler]:
-            handler.setLevel(debug_level)
-            handler.setFormatter(logging.Formatter(
-                '%(asctime)s [%(threadName)s] %(filename)s.%(funcName)-20s %(levelname)s  %(message)s'
-            ))
+    if file_logging:
+        logger.addHandler(file_handler)
 
-        if file_logging:
-            logger.addHandler(file_handler)
+    if console_logging:
+        logger.addHandler(console_handler)
 
-        if console_logging:
-            logger.addHandler(console_handler)
+    return logger
 
-        return logger
+def timer(fn: typing.Callable) -> typing.Callable:
+    """A decorator to measure time performance of functions (garbage-collected).
+
+    Returns the return value of the provided function, if any. 
+
+    Args:
+        fn: Function to start the timer on, can pass in args/kwargs
+    """
+
+    def time_taken(*args, **kwargs):
+        logger = initialize_logger(name="Performance Timer")
+        start_time = time.perf_counter()
+        return_value = fn(*args, **kwargs)
+        end_time = time.perf_counter()
+        time_taken = end_time - start_time
+        logger.info("Time taken for {0}: {1:0.3f}s".format(fn.__name__, time_taken))
+        return return_value
+
+    return time_taken
+
--- test_Youtility.py
+++ test_Youtility.py
@@ -1,28 +1,39 @@
-from Youtility import Youtility
+from Youtility import *
 import os
+
 
 def test_logger_outputs_to_console(capsys):
     # https://docs.pytest.org/en/7.1.x/how-to/capture-stdout-stderr.html#accessing-captured-output-from-a-test-function
-    logger = Youtility.initialize_logger(name='test_logger_outputs_to_console', file_logging=False)
+    logger = initialize_logger(name='test_logger_outputs_to_console', file_logging=False)
 
     logger.info('test logging message')
     console_value = capsys.readouterr().out
 
-    assert '[MainThread] test_Youtility.py.test_logger_outputs_to_console INFO  test logging message\n' in console_value
+    assert 'test logging message' in console_value
 
 def test_logger_outputs_to_file():
     assert os.path.exists('test_debug.log') == False
 
     # Have to use a different logger name as logging.shutdown() is only called atexit
     # https://docs.python.org/3/library/logging.html#logging.shutdown
-    logger = Youtility.initialize_logger(name='test_logger_outputs_to_file', 
+    logger = initialize_logger(name='test_logger_outputs_to_file', 
         filename='test_debug.log', file_arguments={'mode': 'w'})
 
-    logger.info('test logging message')
+    logger.info('test logger outputs to file')
     assert os.path.exists('test_debug.log') == True
 
     with open('test_debug.log', 'r') as file:
         lines = file.readline()
-        assert '[MainThread] test_Youtility.py.test_logger_outputs_to_file INFO  test logging message\n' in lines
+        assert 'test logger outputs to file' in lines
     
     os.remove('test_debug.log')
+
+
+def test_timer(capsys):
+
+    @timer
+    def dummy_function():
+        time.sleep(1)
+    
+    dummy_function()
+    assert 'Time taken for dummy_function' in capsys.readouterr().out
